d76c09bd55be49716b3c140d2542dfee13e7ceb9
ishikhatomar <codebyshikha@gmail.com>
2023-11-02 15:30:50 +0530

    Longest Common Prefix

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+int main(){
+    std::vector<std::string> strs = {"Flower", "Flow", "Flight"};
+    std::string ans = "";
+    int i =0, flag =0;
+    while(i<strs[0].size()){
+        char s = strs[0][i];
+        for(auto x: strs){
+            if(x[i]!=s){
+             flag =1;
+             break; 
+            }
+        }
+        if(flag ==1){
+           std::cout<<ans;
+            break;
+        }
+        else{
+            ans += strs[0][i];
+            i++;
+        }
+
+    }
+    return 0;
+}
