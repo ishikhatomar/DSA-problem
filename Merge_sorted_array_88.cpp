2fe9ed1c2d7f237af65325de7e6cf9b070c03eb9
ishikhatomar <codebyshikha@gmail.com>
2023-11-07 21:18:38 +0530

    merge sorted array

---
+++ Merge_sorted_array_88.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n){
+    int i = m-1;
+    int j=n-1;
+    int k = m+n-1;
+    while(i>=0 && j>=0){
+        if(nums1[i]>=nums2[j]){
+            nums1[k]= nums1[i];
+            i--;
+        }
+        else{
+            nums1[k]=nums2[j];
+            j--;
+        }
+        k--;
+    }
+    while(j>=0){
+        nums1[k]=nums2[j];
+        j--;
+        k--;
+    }
+
+}
+int main(){
+    std::vector<int> nums1 = {1,2,3,0,0,0};
+    std::vector<int> nums2 = {2,3,5};
+    int m=3;
+    int n=3;
+    merge(nums1, m, nums2,n);
+    for(int i: nums1){
+        std::cout<<i<<" ";
+    }
+}
