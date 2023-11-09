b601d1eb99ee7951310a2962b16619f31795ce6c
ishikhatomar <codebyshikha@gmail.com>
2023-11-09 17:42:27 +0530

    excel sheet

--- ExcelSheet_columnNo_171.cpp
+++ ExcelSheet_columnNo_171.cpp
@@ -6,13 +6,9 @@ int titleToNumber(std::string& columnTitle){
     std::reverse(columnTitle.begin(),columnTitle.end());
     int j =1;
     int result =0;
-   std:: unordered_map<int, int> mpp;
-    for(char c= 'A'; c<='Z'; c++){
-        mpp[c]=j;
-        j++;
-    }
+  
     for(int i=0; i<columnTitle.size(); i++){
-      result = result + pow(26,i) * mpp[columnTitle[i]];
+      result = result + pow(26,i) * (columnTitle[i]-'A'+1);
     }
     return result;
 }
